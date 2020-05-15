604a53329d2558e0d6716d4f6502135e0c6deb33
Ayebilla Avoka <40761614+Ayebilla@users.noreply.github.com>
2020-05-15 17:33:06 +0000

    Delete work.py

--- work.py
+++
@@ -1,8 +0,0 @@
-import serial
-import pynmea2
-ser = serial.Serial('/dev/ttyAMA0',9600)
-while 1:
-     data = ser.readline()
-     if (data.startswith("$GPGGA")):
-         msg = pynmea2.parse(data)
-         print repr(msg)
